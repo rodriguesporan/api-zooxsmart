f22fe55c0cc57d9266a197bb555db7dc922a5076
rodriguesporan <rodriguesporan@gmail.com>
2019-06-23 20:46:39 -0300

    Finalizada API

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const homeRoute = require('./routes/homeRoute');
 const statesRoute = require('./routes/statesRoute');
 const citiesroute = require('./routes/citiesRoute');
@@ -11,6 +13,7 @@ const app = express();
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+app.use(cors());
 app.use('/', homeRoute);
 app.use('/states', statesRoute);
 app.use('/cities', citiesroute);
